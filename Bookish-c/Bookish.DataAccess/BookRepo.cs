9dff48e17c20fbd4e8753d70ddf5e0cfb9d08ab0
Arina Demidova <arina.demidova@softwire.com>
2022-11-28 16:48:35 +0000

    Added a Catalogue page; working on displaying the information from the database

---
+++ Bookish-c/Bookish.DataAccess/BookRepo.cs
@@ -0,0 +1,20 @@
+﻿using Bookish.DataAccess;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+using System;
+using Npgsql;
+
+async Task<IEnumerable<Book>> getAllBooks()
+{
+    string conn = "Host=localhost;Port=5432;Username=postgres;Password=_oCoqG5q5L;Database=bookishc";
+    var connection = new NpgsqlConnection(conn);
+    connection.Open();
+
+    var books = await connection.QueryAsync<Book>("select * from bookstable");
+
+    return books;
+}
+
+
+
---
+++ Bookish-c/Bookish.DataAccess/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Bookish.DataAccess
+{
+    public class Book
+    {
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int numOfCopies { get; set; }
+    }
+}
